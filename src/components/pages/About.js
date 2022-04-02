7469794d2f50cdf5e11aa7fc88fc2df3e0dfe0b1
Nicholas Easter <nicholas.easter17@gmail.com>
2022-04-02 16:20:43 -0700

    added cool transition effect to about page tabs when the content is loaded

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -12,30 +12,59 @@ const About = () => {
                 <ul className="flex flex-col md:flex-row flex-wrap ">
                     <li className="mr-1  ">
                         <a href="/" id="con" className=" w-40 md:w-auto inline-block bg-green-400 border-b-2 border-black hover:bg-green-200 focus:bg-green-300 md:rounded-t-lg py-4 px-4 "
-                        onClick={(e) => {
-                            e.preventDefault();
-                            let info = document.querySelector('#experience');
-                            console.log(info);
-                            (info.style.display === "none") ? info.style.display = 'block' : info.style.display = 'none'; 
-                        }}
+                            onClick={(e) => {
+                                e.preventDefault();
+                                document.querySelector('#technologies').style.display = "none";
+                                document.querySelector('#education').style.display = "none";
+                                // this toggles the display of the nfo in the experience div
+                                let info = document.querySelector('#experience');
+                                console.log(info);
+                                (info.style.display === "none") ? info.style.display = 'block' : info.style.display = 'none';
+                            }}
                         >
                             Experience
                         </a>
-                        <div id="experience" className=" after:transition ease-in duration-500 " 
-                            
+                        <div id="experience" className="animate"
+
                         > TRANSITION </div>
                     </li>
-                    <li className="mr-1">
-                        <a href="#" className=" w-40 md:w-auto inline-block bg-green-400 border-b-2 border-black hover:bg-green-200 focus:bg-green-300 md:rounded-t-lg py-4 px-4 ">
+                    <li className="mr-1  ">
+                        <a href="/" id="con" className=" w-40 md:w-auto inline-block bg-green-400 border-b-2 border-black hover:bg-green-200 focus:bg-green-300 md:rounded-t-lg py-4 px-4 "
+                            onClick={(e) => {
+                                e.preventDefault();
+                                document.querySelector('#experience').style.display = "none";
+                                document.querySelector('#education').style.display = "none";
+                                // this toggles the display of the nfo in the technologies div
+                                let info = document.querySelector('#technologies');
+                                console.log(info);
+                                (info.style.display === "none") ? info.style.display = 'block' : info.style.display = 'none';
+                            }}
+                        >
                             Technologies
                         </a>
+                        <div id="technologies" className="animate"
+
+                        > TRANSITION </div>
                     </li>
-                    <li className="mr-1">
-                        <a href="#" className=" w-40 md:w-auto inline-block bg-green-400 border-b-2 border-black hover:bg-green-200 focus:bg-green-300 md:rounded-t-lg py-4 px-4 ">
+                    <li className="mr-1  ">
+                        <a href="/" id="con" className=" w-40 md:w-auto inline-block bg-green-400 border-b-2 border-black hover:bg-green-200 focus:bg-green-300 md:rounded-t-lg py-4 px-4 "
+                            onClick={(e) => {
+                                e.preventDefault();
+                                document.querySelector('#technologies').style.display = "none";
+                                document.querySelector('#experience').style.display = "none";
+                                // this toggles the display of the nfo in the education div
+                                let info = document.querySelector('#education');
+                                console.log(info);
+                                (info.style.display === "none") ? info.style.display = 'block' : info.style.display = 'none';
+                            }}
+                        >
                             Education
                         </a>
+                        <div id="education" className=" animate"
+
+                        > TRANSITION </div>
                     </li>
-                        
+
                 </ul>
             </div>
         </div>
