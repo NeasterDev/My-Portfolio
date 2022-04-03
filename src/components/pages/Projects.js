952067ffcfd82085332193d222fb07131ffbcb9f
Nicholas Easter <nicholas.easter17@gmail.com>
2022-04-03 11:24:38 -0700

    made the project cards more responsive

--- src/components/pages/Projects.js
+++ src/components/pages/Projects.js
@@ -9,23 +9,23 @@ const Projects = () => {
                     Projects
                 </h1>
             </div>
-            <div className="flex flex-col flex-wrap items-center">
-                <div className="flex-grow mb-4 w-2/3 border-[16px] border-green-900 hover:border-green-400">
+            <div className="flex flex-col md:flex-row flex-wrap items-center lg:justify-start">
+                <div className="flex-grow mb-4 w-2/3 md:w-1/3 lg:w-1/5 md:m-12 border-[16px] border-green-900 hover:border-green-400">
                     <ProjectCard imgLink="https://via.placeholder.com/300/09f/fff.png" title="Project 1" github="www.github.com"  />
                 </div>
-                <div className=" flex-grow mb-4 w-2/3 border-[16px] border-green-900 hover:border-green-400">
+                <div className=" flex-grow mb-4 w-2/3 md:w-1/3 lg:w-1/5 md:m-12 border-[16px] border-green-900 hover:border-green-400">
                     <ProjectCard imgLink="https://via.placeholder.com/300/09f/fff.png" title="Project 1" github="www.github.com"  />
                 </div>
-                <div className=" flex-grow mb-4 w-2/3 border-[16px] border-green-900 hover:border-green-400">
+                <div className=" flex-grow mb-4 w-2/3 md:w-1/3 lg:w-1/5 md:m-12 border-[16px] border-green-900 hover:border-green-400">
                     <ProjectCard imgLink="https://via.placeholder.com/300/09f/fff.png" title="Project 1" github="www.github.com"  />
                 </div>
-                <div className=" flex-grow mb-4 w-2/3 border-[16px] border-green-900 hover:border-green-400">
+                <div className=" flex-grow mb-4 w-2/3 md:w-1/3 lg:w-1/5 md:m-12 border-[16px] border-green-900 hover:border-green-400">
                     <ProjectCard imgLink="https://via.placeholder.com/300/09f/fff.png" title="Project 1" github="www.github.com"  />
                 </div>
-                <div className=" flex-grow mb-4 w-2/3 border-[16px] border-green-900 hover:border-green-400">
+                <div className=" flex-grow mb-4 w-2/3 md:w-1/3 lg:w-1/5 md:m-12 border-[16px] border-green-900 hover:border-green-400">
                     <ProjectCard imgLink="https://via.placeholder.com/300/09f/fff.png" title="Project 1" github="www.github.com"  />
                 </div>
-                <div className=" flex-grow mb-4 w-2/3 border-[16px] border-green-900 hover:border-green-400">
+                <div className=" flex-grow mb-4 w-2/3 md:w-1/3 lg:w-1/5 md:m-12 border-[16px] border-green-900 hover:border-green-400">
                     <ProjectCard imgLink="https://via.placeholder.com/300/09f/fff.png" title="Project 1" github="www.github.com"  />
                 </div>
             </div>
