6408a0de09e5ea5dd0288141f5603871d26b905b
Nicholas Easter <nicholas.easter17@gmail.com>
2022-04-06 11:19:01 -0700

    finished project cards, just need to update links and images to websites i created

--- src/components/pages/ProjectCard.js/index.js
+++ src/components/pages/ProjectCard.js/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import GithubLogo from "../../../images/GitHub-Mark-64px.png";
 import GithubLogo2 from "../../../images/GitHub_Logo.png";
 
-const ProjectCard = ({ title, github, imgLink }) => {
+const ProjectCard = ({ title, github, imgLink, websiteLink }) => {
   const myStyle = {
     backgroundImage: `url(${imgLink})`,
     backgroundSize: "100% 100%",
@@ -10,18 +10,16 @@ const ProjectCard = ({ title, github, imgLink }) => {
 
   return (
     <div
-      className="relative bg-no-repeat flex flex-col border border-black justify-center items-center h-96 rounded-lg"
+      className="bg-no-repeat flex flex-col border border-black justify-end items-center h-96 rounded-lg"
       style={myStyle}
     >
       <div className="">
-        <span className="text-7xl font-bold ">{title}</span>
+        <a href={websiteLink} className="text-7xl font-bold opacity-40 hover:opacity-100 cursor-pointer">{title}</a>
       </div>
-      <div className="absolute bottom-7 right-7 opacity-50 hover:opacity-100">
-        <a href={github} className="">
-          <span className="flex justify-end">
+      <div className=" opacity-30 hover:opacity-100">
+        <a href={github} className="flex justify-center">
             <img src={GithubLogo} alt="github logo" />
             <img className=" w-1/5" src={GithubLogo2} alt="github logo" />
-          </span>
         </a>
       </div>
     </div>
--- src/components/pages/Projects.js
+++ src/components/pages/Projects.js
@@ -11,16 +11,16 @@ const Projects = () => {
             </div>
             <div className="flex flex-col md:flex-row flex-wrap items-center lg:justify-start">
                 <div className="flex-grow mb-4 w-2/3 lg:w-1/5 2xl:w-3/12 md:m-12 opacity-75 hover:opacity-100 hover:shadow-2xl rounded-lg">
-                    <ProjectCard imgLink="https://via.placeholder.com/300/09f/fff.png" title="Project 1" github="www.github.com"  />
+                    <ProjectCard imgLink="https://via.placeholder.com/300/09f/fff.png" title="Amazing Website" github="www.github.com"  />
                 </div>
                 <div className=" flex-grow mb-4 w-2/3 lg:w-1/5 2xl:w-3/12 md:m-12 opacity-75 hover:opacity-100 hover:shadow-2xl hover:border-green-400 rounded-lg">
-                    <ProjectCard imgLink="https://via.placeholder.com/300/09f/fff.png" title="Project 1" github="www.github.com"  />
+                    <ProjectCard imgLink="https://via.placeholder.com/300/09f/fff.png" title="InstShoe" github="www.github.com"  />
                 </div>
                 <div className=" flex-grow mb-4 w-2/3 lg:w-1/5 2xl:w-3/12 md:m-12 opacity-75 hover:opacity-100 hover:shadow-2xl hover:border-green-400 rounded-lg">
-                    <ProjectCard imgLink="https://via.placeholder.com/300/09f/fff.png" title="Project 1" github="www.github.com"  />
+                    <ProjectCard imgLink="https://via.placeholder.com/300/09f/fff.png" title="Random Website" github="www.github.com"  />
                 </div>
                 <div className=" flex-grow mb-4 w-2/3 lg:w-1/5 2xl:w-3/12 md:m-12 opacity-75 hover:opacity-100 hover:shadow-2xl hover:border-green-400 rounded-lg">
-                    <ProjectCard imgLink="https://via.placeholder.com/300/09f/fff.png" title="Project 1" github="www.github.com"  />
+                    <ProjectCard imgLink="https://via.placeholder.com/300/09f/fff.png" title="Website" github="www.github.com"  />
                 </div>
                 <div className=" flex-grow mb-4 w-2/3 lg:w-1/5 2xl:w-3/12 md:m-12 opacity-75 hover:opacity-100 hover:shadow-2xl hover:border-green-400 rounded-lg">
                     <ProjectCard imgLink="https://via.placeholder.com/300/09f/fff.png" title="Project 1" github="www.github.com"  />
