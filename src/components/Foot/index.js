95d76046e34f8368092251f035870ceda533ce14
Nicholas Easter <nicholas.easter17@gmail.com>
2022-04-06 12:32:35 -0700

    added linkedin github and stackshare links to footer

--- src/components/Foot/index.js
+++ src/components/Foot/index.js
@@ -6,9 +6,9 @@ const Foot = () => {
             {/** Footer Begin */}
             <footer className=" mt-4 h-14 bg-green-200 shadow-lg">
                 <div className="flex justify-center">
-                    <a href="/" className=" my-5"><span className=" mx-5 italic text-green-900 hover:text-black">LinkedIn</span></a>
-                    <a href="/" className=" my-5"><span className=" mx-5 italic text-green-900 hover:text-black">GitHub</span></a>
-                    <a href="/" className=" my-5"><span className=" mx-5 italic text-green-900 hover:text-black">Stackshare</span></a>
+                    <a href="https://www.linkedin.com/in/nicholas-easter-b04182222/" className=" my-5"><span className=" mx-5 italic text-green-900 hover:text-black">LinkedIn</span></a>
+                    <a href="https://github.com/crazypants300" className=" my-5"><span className=" mx-5 italic text-green-900 hover:text-black">GitHub</span></a>
+                    <a href="https://stackshare.io/nicholaseaster17" className=" my-5"><span className=" mx-5 italic text-green-900 hover:text-black">Stackshare</span></a>
                 </div>
             </footer>
         </div>
