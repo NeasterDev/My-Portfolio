70ceb0dff61d5d868d1be0570df4eb43898deead
Nicholas Easter <nicholas.easter17@gmail.com>
2022-04-10 10:01:41 -0700

    added resume download link with placeholder resume

--- src/components/pages/Resume.js
+++ src/components/pages/Resume.js
@@ -1,4 +1,5 @@
 import React from "react";
+import resume from '../../files/resume.txt'
 
 const Resume = () => {
     return (
@@ -7,6 +8,11 @@ const Resume = () => {
                 <h1 className=" md:ml-10 cursor-default text-shadow md:text-8xl text-6xl text-left text-green-400 hover:text-green-500   ">
                     Resume
                 </h1>
+            </div>
+            <div className="flex justify-center">
+                <div className="bg-green-400 hover:bg-green-200 rounded-md p-4 cursor-pointer">
+                    <a href={resume} download="Nicholas_easter_resume">Download Resume</a>
+                </div>
             </div>
         </div>
     );
