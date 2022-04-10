1793450efea93253edc756fd46d42dec29c5fe4e
Nicholas Easter <nicholas.easter17@gmail.com>
2022-04-10 10:20:36 -0700

    finished project cards

--- src/components/pages/ProjectCard.js/index.js
+++ src/components/pages/ProjectCard.js/index.js
@@ -17,8 +17,8 @@ const ProjectCard = ({ title, github, imgLink, websiteLink }) => {
       <div className=" bg-black-rgba-0 hover:bg-black-rgba-1 px-4 pt-2 rounded-sm">
         <a href={websiteLink} target="_blank" className="text-3xl font-bold opacity-60 hover:opacity-100 text-shadow text-green-600 hover:text-green-300 cursor-pointer">{title}</a>
       </div>
-      <div className="bg-black-rgba-0 hover:bg-black-rgba-1 px-4 pt-2 rounded-sm opacity-30 hover:opacity-100">
-        <a href={github} className="flex justify-center w-1/2 m-auto">
+      <div className=" bg-black-rgba-0 hover:bg-black-rgba-1 px-4 pt-2 rounded-sm opacity-30 hover:opacity-100">
+        <a href={github} className="flex justify-center w-1/2 mx-auto mb-4">
           <img className="" src={GithubLogo} alt="github logo" />
           <div className="flex">
             <img className="" src={GithubLogo2} alt="github logo" />
--- src/components/pages/Projects.js
+++ src/components/pages/Projects.js
@@ -16,22 +16,22 @@ const Projects = () => {
                 </h1>
             </div>
             <div className="flex flex-col md:flex-row flex-wrap items-center lg:justify-start">
-                <div className="flex-grow mb-4 w-2/3 lg:w-1/5 2xl:w-3/12 md:m-12 opacity-75 hover:opacity-100 hover:shadow-2xl rounded-lg">
+                <div className="flex-grow mb-4 w-11/12 lg:w-1/5 2xl:w-3/12 md:m-12 opacity-75 hover:opacity-100 hover:shadow-2xl rounded-lg">
                     <ProjectCard imgLink={instaShoe} title="InstaShoe" websiteLink="https://shoe-production.herokuapp.com/home" github="www.github.com"  />
                 </div>
-                <div className=" flex-grow mb-4 w-2/3 lg:w-1/5 2xl:w-3/12 md:m-12 opacity-75 hover:opacity-100 hover:shadow-2xl hover:border-green-400 rounded-lg">
+                <div className=" flex-grow mb-4 w-11/12 lg:w-1/5 2xl:w-3/12 md:m-12 opacity-75 hover:opacity-100 hover:shadow-2xl hover:border-green-400 rounded-lg">
                     <ProjectCard imgLink={weatherDashboard} title="Weather Dashboard" websiteLink="https://crazypants300.github.io/weather-dashboard/" github="www.github.com"  />
                 </div>
-                <div className=" flex-grow mb-4 w-2/3 lg:w-1/5 2xl:w-3/12 md:m-12 opacity-75 hover:opacity-100 hover:shadow-2xl hover:border-green-400 rounded-lg">
+                <div className=" flex-grow mb-4 w-11/12 lg:w-1/5 2xl:w-3/12 md:m-12 opacity-75 hover:opacity-100 hover:shadow-2xl hover:border-green-400 rounded-lg">
                     <ProjectCard imgLink={taskmaster} title="Taskmaster Pro" websiteLink="https://crazypants300.github.io/taskmaster-pro/" github="www.github.com"  />
                 </div>
-                <div className=" flex-grow mb-4 w-2/3 lg:w-1/5 2xl:w-3/12 md:m-12 opacity-75 hover:opacity-100 hover:shadow-2xl hover:border-green-400 rounded-lg">
+                <div className=" flex-grow mb-4 w-11/12 lg:w-1/5 2xl:w-3/12 md:m-12 opacity-75 hover:opacity-100 hover:shadow-2xl hover:border-green-400 rounded-lg">
                     <ProjectCard imgLink={workScheduler} title="Work Scheduler" websiteLink="https://crazypants300.github.io/daily-planner/Develop/index.html" github="www.github.com"  />
                 </div>
-                <div className=" flex-grow mb-4 w-2/3 lg:w-1/5 2xl:w-3/12 md:m-12 opacity-75 hover:opacity-100 hover:shadow-2xl hover:border-green-400 rounded-lg">
+                <div className=" flex-grow mb-4 w-11/12 lg:w-1/5 2xl:w-3/12 md:m-12 opacity-75 hover:opacity-100 hover:shadow-2xl hover:border-green-400 rounded-lg">
                     <ProjectCard imgLink={runBuddy} title="Run Buddy" websiteLink="https://crazypants300.github.io/run-buddy/" github="www.github.com"  />
                 </div>
-                <div className=" flex-grow mb-4 w-2/3 lg:w-1/5 2xl:w-3/12 md:m-12 opacity-75 hover:opacity-100 hover:shadow-2xl hover:border-green-400 rounded-lg">
+                <div className=" flex-grow mb-4 w-11/12 lg:w-1/5 2xl:w-3/12 md:m-12 opacity-75 hover:opacity-100 hover:shadow-2xl hover:border-green-400 rounded-lg">
                     <ProjectCard imgLink={quiz} title="Coding Quiz" websiteLink="https://crazypants300.github.io/coding-quiz/" github="www.github.com"  />
                 </div>
             </div>
