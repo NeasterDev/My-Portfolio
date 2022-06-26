ada61a57a0008a570399d524e429c3ef85cfe78b
Nicholas Easter <nicholas.easter17@gmail.com>
2022-06-25 18:22:44 -0700

    updated technical skills to be more readable

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -1,5 +1,5 @@
 import React from "react";
-import selfie from "../../images/selfie-min.png"; 
+import selfie from "../../images/selfie-min.png";
 import resume from '../../files/Resume.pdf';
 
 const About = () => {
@@ -10,15 +10,50 @@ const About = () => {
                             where I learned industry proven techniques to develop high-quality, mobile-friendly applications. 
                             `
 
-    const technologiesText = `I am proficient: React, HTML5, CSS3, JavaScript, ES6, Node.js, Express.js, MySQL, SQL, MongoDB, GraphQL, NPM, Markdown, and RegEx `;
-    
-    const downloadResume = (
-        <div className="flex justify-center">
-                <div className="bg-green-400 hover:bg-green-200 rounded-md p-4 cursor-pointer">
-                    <a href={resume} download="Nicholas_easter_resume">Download Resume</a>
+    // const technologiesText = `I am proficient: React, HTML5, CSS3, JavaScript, ES6, Node.js, Express.js, MySQL, SQL, MongoDB, GraphQL, NPM, Markdown, and RegEx `;
+
+    const technologiesText = () => {
+        return (
+            <div>
+                <div className=" bg-green-400">
+                    <div className="flex flex-col items-center p-4 border-slate-400 border-2 shadow-lg">
+                        <h1 className="md:text-4xl ">Frontend Skills:</h1>
+                        <div>
+                            <ol>
+                                <li>React</li>
+                                <li>HTML5</li>
+                                <li>CSS3</li>
+                                <li>JavaScript</li>
+                                <li>Bootstrap</li>
+                                <li>Tailwind</li>
+                            </ol>
+                        </div>
+                    </div>
+                </div>
+                <div className=" bg-green-400">
+                    <div className="flex flex-col items-center p-4 border-slate-400 border-2 shadow-lg mt-4">
+                        <h1 className="md:text-4xl ">Backend Skills:</h1>
+                        <div>
+                            <ol>
+                                <li>NodeJS</li>
+                                <li>ExpressJS</li>
+                                <li>MySQL</li>
+                                <li>MongoDB</li>
+                                <li>GraphQL</li>
+                            </ol>
+                        </div>
+                    </div>
                 </div>
             </div>
-        );
+        )
+    }
+    const downloadResume = (
+        <div className="flex justify-center">
+            <div className="bg-green-400 hover:bg-green-200 rounded-md p-4 cursor-pointer">
+                <a href={resume} download="Nicholas_easter_resume">Download Resume</a>
+            </div>
+        </div>
+    );
 
 
     return (
@@ -34,22 +69,22 @@ const About = () => {
                         <a href="/" className=" w-40 md:w-auto inline-block bg-green-400 border-b-2 border-black hover:bg-green-200 focus:bg-green-300 rounded-t-lg py-4 px-4 "
                             onClick={(e) => {
                                 e.preventDefault();
-                                
+
                                 document.querySelector('#technologies').style.display = "none";
                                 document.querySelector('#default').style.display = "none";
                                 document.querySelector('#education').style.display = "none";
                                 // this toggles the display of the nfo in the experience div
                                 let info = document.querySelector('#experience');
-                                if(!info.style.display) {
+                                if (!info.style.display) {
                                     info.style.display = "none";
                                 }
                                 console.log(info);
                                 (info.style.display === "none") ? info.style.display = 'block' : info.style.display = 'none';
-                                if (document.querySelector('#technologies').style.display === "none" 
+                                if (document.querySelector('#technologies').style.display === "none"
                                     && document.querySelector('#education').style.display === "none"
                                     && document.querySelector('#experience').style.display === "none") {
-                                        document.querySelector('#default').style.display = "block";
-                                    }
+                                    document.querySelector('#default').style.display = "block";
+                                }
                             }}
                         >
                             About me
@@ -60,23 +95,23 @@ const About = () => {
                         <a href="/" className=" w-40 md:w-auto inline-block bg-green-400 border-b-2 border-black hover:bg-green-200 focus:bg-green-300 md:rounded-t-lg py-4 px-4 "
                             onClick={(e) => {
                                 e.preventDefault();
-                                
+
                                 document.querySelector('#experience').style.display = "none";
                                 document.querySelector('#default').style.display = "none";
                                 document.querySelector('#education').style.display = "none";
                                 // this toggles the display of the nfo in the technologies div
                                 let info = document.querySelector('#technologies');
                                 // checks if the display property has been set (ie the first click)
-                                if(!info.style.display) {
+                                if (!info.style.display) {
                                     info.style.display = "none";
                                 }
                                 console.log(info);
                                 (info.style.display === "none") ? info.style.display = 'block' : info.style.display = 'none';
-                                if (document.querySelector('#technologies').style.display === "none" 
+                                if (document.querySelector('#technologies').style.display === "none"
                                     && document.querySelector('#education').style.display === "none"
                                     && document.querySelector('#experience').style.display === "none") {
-                                        document.querySelector('#default').style.display = "block";
-                                    }
+                                    document.querySelector('#default').style.display = "block";
+                                }
                             }}
                         >
                             Technical Skills
@@ -84,42 +119,42 @@ const About = () => {
 
                     </li>
                     <li className="mr-1  ">
-                        <a href="/"  className=" w-40 md:w-auto inline-block bg-green-400 border-b-2 border-black hover:bg-green-200 focus:bg-green-300 md:rounded-t-lg py-4 px-4 "
+                        <a href="/" className=" w-40 md:w-auto inline-block bg-green-400 border-b-2 border-black hover:bg-green-200 focus:bg-green-300 md:rounded-t-lg py-4 px-4 "
                             onClick={(e) => {
                                 e.preventDefault();
-                                
+
                                 document.querySelector('#technologies').style.display = "none";
                                 document.querySelector('#default').style.display = "none";
                                 document.querySelector('#experience').style.display = "none";
                                 // this toggles the display of the nfo in the education div
                                 let info = document.querySelector('#education');
-                                if(!info.style.display) {
+                                if (!info.style.display) {
                                     info.style.display = "none";
                                 }
                                 console.log(info);
                                 (info.style.display === "none") ? info.style.display = 'block' : info.style.display = 'none';
-                                if (document.querySelector('#technologies').style.display === "none" 
+                                if (document.querySelector('#technologies').style.display === "none"
                                     && document.querySelector('#education').style.display === "none"
                                     && document.querySelector('#experience').style.display === "none") {
-                                        document.querySelector('#default').style.display = "block";
-                                    }
+                                    document.querySelector('#default').style.display = "block";
+                                }
                             }}
                         >
                             Resume
                         </a>
                     </li>
                 </ul>
-                
+
                 <div className="mt-4 flex flex-col md:flex-row">
                     <div id="experience" className="animate hidden">  {experienceText}  </div>
-                    <div id="technologies" className="animate hidden">  {technologiesText} </div>
+                    <div id="technologies" className="animate hidden w-full ">  {technologiesText()} </div>
                     <div id="education" className="animate hidden w-full md:pr-11rem">  {downloadResume} </div>
                     <div id="default" className="animate">  {experienceText} </div>
                     <div className="w-full md:w-1/2 mt-2 md:mt-0 md:ml-6 md:mb-4 self-center md:self-start md:flex-shrink-0">
-                        <img className="mx-auto border-[10px] border-green-400 shadow-2xl" src={selfie} alt="Nicholas Easter"/>
+                        <img className="mx-auto border-[10px] border-green-400 shadow-2xl" src={selfie} alt="Nicholas Easter" />
                     </div>
                 </div>
-                
+
             </div>
         </div>
     );
